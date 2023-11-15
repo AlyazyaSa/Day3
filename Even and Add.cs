d962f27b27b3302b88bafac4c2ae3afdf1417e96
Alyazyiah Saif <93020498+AlyazyaSa@users.noreply.github.com>
2023-11-15 13:32:50 +0400

    Add files via upload

---
+++ Even and Add.cs
@@ -0,0 +1,14 @@
+﻿namespace REFERNCE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            string result = (x % 2 == 0) ? " x is even" : "x is odd";
+            Console.WriteLine(result);
+
+            //string result(x<y);
+        }
+    } 
+}
